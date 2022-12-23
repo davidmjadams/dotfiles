14051d032e73be3ad7b42ce649f517e3ced34c3e
David <david@davids-mbp.lan>
2022-12-23 22:45:04 +0000

    first commit

---
+++ brew-cask.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew cleanup
+
+
+# Install native apps
+
+# daily
+brew install --cask readdle-spark
+brew install --cask 1password
+brew install --cask brave-browser
+brew install --cask slack
+brew install --cask cron
+brew install --cask figma
+brew install --cask visual-studio-code
+brew install --cask postman
+brew install --cask alfred
+
+# dev
+brew install --cask sublime-text
+brew install --cask imageoptim
+
+
+# less often
+brew install --cask disk-inventory-x
+brew install --cask screenflow
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code
---
+++ brew.sh
@@ -0,0 +1,68 @@
+	#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# (Optionally) Turn off brew's analytics https://docs.brew.sh/Analytics
+# brew analytics off
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
+brew install findutils
+# GNU `sed`
+brew install gnu-sed
+
+
+
+# Updated shells
+# Note: don’t forget to add `/usr/local/bin/<EACHSHELL>` to `/etc/shells` before running `chsh`.
+brew install bash
+brew install zsh
+
+brew install bash-completion@2
+
+
+# Install wget
+brew install wget
+
+# Install more recent versions of some OS X tools
+brew install vim
+brew install nano
+brew install grep
+brew install openssh
+
+# z hopping around folders
+brew install z
+
+
+# github util
+brew install gh
+# nicer git diffs
+brew install git-delta
+brew install git
+
+brew install shellcheck # linting for .sh files
+
+
+# Install other useful binaries
+brew install imagemagick
+brew install node # This installs `npm` too using the recommended installation method
+
+# json stuff
+brew install jq gron
+
+brew install ncdu # find where your diskspace went
+
+# will probably need these at some point
+# brew install automake cmake go rust
+
+# Remove outdated versions from the cellar
+brew cleanup
